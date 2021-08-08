429cabe4dbb56a1d425f90dafe67d9d0b0b18041
Alexander Czigler <alexander@iteam.se>
2021-08-08 15:25:18 +0200

    feat: logo for now

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,46 +5,36 @@
 	<title>Bäcklidens Egyptian Mao</title>
 </svelte:head>
 
-<h1>Bäcklidens</h1>
-<h2>Egyptian Mao</h2>
-
 <p>
-	<strong>
-		Try editing this file (src/routes/index.svelte) to test live reloading.
-	</strong>
+	<strong> Välkommen! </strong>
+</p>
+<p>
+	<strong> Välkommen! </strong>
+</p>
+<p>
+	<strong> Välkommen! </strong>
+</p>
+<p>
+	<strong> Välkommen! </strong>
+</p>
+<p>
+	<strong> Välkommen! </strong>
+</p>
+<p>
+	<strong> Välkommen! </strong>
+</p>
+<p>
+	<strong> Välkommen! </strong>
+</p>
+<p>
+	<strong> Välkommen! </strong>
 </p>
 
 <style>
-	h1,
-	p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 1.1em;
-		font-weight: 400;
-		margin: 0 0 0 0;
-	}
-
-	h2 {
-		font-size: 1.1em;
-		margin: 0 0 0.5em 0;
-	}
-
 	p {
 		margin: 1em auto;
 	}
 
 	@media (min-width: 480px) {
-		h1 {
-			font-size: 3em;
-			font-weight: 600;
-		}
-
-		h2 {
-			font-size: 1.5em;
-			font-weight: 100;
-		}
 	}
 </style>
