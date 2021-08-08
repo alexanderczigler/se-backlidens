ea07c1ff0da9ac2206402e105a74ecdd3ba742cd
Alexander Czigler <alexander@iteam.se>
2021-08-08 16:15:39 +0200

    feat: more basic layouting

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,34 +5,34 @@
 	<title>Bäcklidens Egyptian Mao</title>
 </svelte:head>
 
+<liza />
+
+<h3>
+	<strong>Välkommen!</strong>
+</h3>
 <p>
-	<strong> Välkommen! </strong>
+	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab fugit voluptatum
+	quibusdam deserunt fuga, eligendi quos reiciendis laboriosam doloribus, harum
+	nulla a numquam, nam sapiente placeat tempora eos debitis tempore!
 </p>
 <p>
-	<strong> Välkommen! </strong>
-</p>
-<p>
-	<strong> Välkommen! </strong>
-</p>
-<p>
-	<strong> Välkommen! </strong>
-</p>
-<p>
-	<strong> Välkommen! </strong>
-</p>
-<p>
-	<strong> Välkommen! </strong>
-</p>
-<p>
-	<strong> Välkommen! </strong>
-</p>
-<p>
-	<strong> Välkommen! </strong>
+	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus quia natus
+	pariatur corporis cumque dolores consequuntur nobis harum. Magnam, veniam
+	quas. Non, eaque omnis. Cumque optio dicta tempore quae! Cum.
 </p>
 
 <style>
-	p {
-		margin: 1em auto;
+	liza {
+		display: block;
+		background-image: url("/liza.jpg");
+		background-position: center center;
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-size: cover;
+		background-color: var(--falu);
+		margin: 0;
+		height: 24em;
+		width: 100%;
 	}
 
 	@media (min-width: 480px) {
