7b0750cf3f7e78dfe677aa3e09a380747be84009
Alexander Czigler <alexander@iteam.se>
2021-08-09 12:50:57 +0200

    fix: click logic

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,78 +1,66 @@
 <script>
 	export let segment;
+	export let toggleMenu;
 </script>
 
-<ul>
-	<li>
-		<a aria-current={segment === undefined ? "page" : undefined} href=".">hem</a
-		>
-	</li>
-	<li>
-		<a aria-current={segment === "cats" ? "page" : undefined} href="cats"
-			>katter</a
-		>
-	</li>
-	<li>
-		<a aria-current={segment === "litters" ? "page" : undefined} href="litters"
-			>kullar</a
-		>
-	</li>
-	<li>
-		<a aria-current={segment === "shows" ? "page" : undefined} href="shows"
-			>utställningar</a
-		>
-	</li>
-</ul>
+<nav>
+	<ul>
+		<li>
+			<a
+				on:click={toggleMenu}
+				aria-current={segment === undefined ? "page" : undefined}
+				href=".">hem</a
+			>
+		</li>
+		<li>
+			<a
+				on:click={toggleMenu}
+				aria-current={segment === "cats" ? "page" : undefined}
+				href="cats">katter</a
+			>
+		</li>
+		<li>
+			<a
+				on:click={toggleMenu}
+				aria-current={segment === "litters" ? "page" : undefined}
+				href="litters">kullar</a
+			>
+		</li>
+		<li>
+			<a
+				on:click={toggleMenu}
+				aria-current={segment === "shows" ? "page" : undefined}
+				href="shows">utställningar</a
+			>
+		</li>
+	</ul>
+</nav>
 
 <style>
-	nav {
+	ul {
+		list-style-type: none;
+	}
+
+	li {
+		color: var(--lotion);
+		font-size: 2.6em;
+		font-weight: 400;
 		text-transform: uppercase;
-		background-color: var(--lotion);
-		height: 5em;
+		text-align: right;
 	}
 
-	overlay {
+	[aria-current] {
+		font-weight: 600;
+		color: var(--grayscale);
+	}
+
+	a {
+		text-decoration: none;
+		padding: 0 0.5em;
 		display: block;
-		position: absolute;
-		height: 100%;
-		width: 100%;
-		top: 5em;
-		background-color: var(--falu);
-		z-index: 10;
-	}
-
-	options {
-		display: none;
-	}
-
-	hamburger {
-		float: right;
-	}
-
-	hamburger > span > i {
-		font-size: 2em;
-		padding-right: 0.9em;
-		padding-top: 0.7em;
 	}
 
 	@media (min-width: 800px) {
-		hamburger {
-			display: none;
-		}
-
-		nav {
-			position: relative;
-			display: block;
-			top: 0em;
-			width: 100%;
-			padding: 1em 0;
-			height: 2em;
-		}
-
-		options {
-			display: block;
-		}
-
 		ul {
 			float: right;
 			margin: 0.3em 1em 0 0;
@@ -87,6 +75,7 @@
 		}
 
 		li {
+			color: var(--grayscale);
 			font-weight: 100;
 			font-size: 1em;
 			display: block;
