1d98cb65dd6229db5df41b26c6074795ef541515
Alexander Czigler <alexander@iteam.se>
2021-08-09 12:34:50 +0200

    chore: rearranging things

---
+++ src/sections/Top.svelte
@@ -0,0 +1,83 @@
+<script>
+  import Logo from "../components/Logo.svelte";
+  import Nav from "../components/Nav.svelte";
+
+  export let segment;
+
+  let overlay = false;
+  const toggleMenu = () => {
+    overlay = !overlay;
+  };
+</script>
+
+{#if overlay}
+  <overlay>
+    <h3>Meny</h3>
+    <Nav {segment} />
+  </overlay>
+{/if}
+
+<nav>
+  <Logo />
+
+  <hamburger>
+    <span>
+      <i class="fa fa-bars" on:click={toggleMenu} />
+    </span>
+  </hamburger>
+
+  <options>
+    <Nav {segment} />
+  </options>
+</nav>
+
+<style>
+  nav {
+    text-transform: uppercase;
+    background-color: var(--lotion);
+    height: 5em;
+  }
+
+  overlay {
+    display: block;
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 5em;
+    background-color: var(--falu);
+    z-index: 10;
+  }
+
+  options {
+    display: none;
+  }
+
+  hamburger {
+    float: right;
+  }
+
+  hamburger > span > i {
+    font-size: 2em;
+    padding-right: 0.9em;
+    padding-top: 0.7em;
+  }
+
+  @media (min-width: 800px) {
+    hamburger {
+      display: none;
+    }
+
+    nav {
+      position: relative;
+      display: block;
+      top: 0em;
+      width: 100%;
+      padding: 1em 0;
+      height: 2em;
+    }
+
+    options {
+      display: block;
+    }
+  }
+</style>
