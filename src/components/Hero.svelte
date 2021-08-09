3f90376636bd2afe6d03a1512d04a5aa1ffca57b
Alexander Czigler <alexander@iteam.se>
2021-08-09 13:38:19 +0200

    feat: hero styling

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -3,14 +3,36 @@
 <style>
   hero {
     display: block;
-    background-image: url("/liza.jpg");
-    background-position: center center;
-    background-repeat: no-repeat;
     background-attachment: fixed;
-    background-size: cover;
     background-color: var(--falu);
+    background-image: url("/liza-mobile.jpg");
+    background-position: 0 -5em;
+    background-repeat: no-repeat;
+    background-size: 100%;
     margin: 0;
-    height: 24em;
+    height: 9em;
     width: 100%;
+
+    -moz-box-shadow: inset 0 0 10px var(--grayscale);
+    -webkit-box-shadow: inset 0 0 10px var(--grayscale);
+    box-shadow: inset 0px 11px 8px -10px var(--grayscale),
+      inset 0px -11px 8px -10px var(--grayscale);
+  }
+
+  @media (min-width: 800px) {
+    hero {
+      background-image: url("/liza.jpg");
+      background-position: 0 -12em;
+      background-size: cover;
+      height: 24em;
+    }
+  }
+
+  @media (min-width: 1400px) {
+    hero {
+      background-image: url("/liza.jpg");
+      background-position: 0 4em;
+      height: 24em;
+    }
   }
 </style>
