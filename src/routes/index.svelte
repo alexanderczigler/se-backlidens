860828d2fcddae38ec3c9c544ea78439c4a8a953
Alexander Czigler <alexander@iteam.se>
2021-08-10 16:18:55 +0200

    feat: new page structure

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,18 +8,41 @@
 
 <Hero />
 
-<h3>
-	<strong>Välkommen!</strong>
-</h3>
+<h3>Välkommen!</h3>
 <p>
 	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab fugit voluptatum
 	quibusdam deserunt fuga, eligendi quos reiciendis laboriosam doloribus, harum
 	nulla a numquam, nam sapiente placeat tempora eos debitis tempore!
 </p>
+
+<h3>Om oss</h3>
 <p>
-	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus quia natus
-	pariatur corporis cumque dolores consequuntur nobis harum. Magnam, veniam
-	quas. Non, eaque omnis. Cumque optio dicta tempore quae! Cum.
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, modi explicabo
+	sint facilis ratione commodi inventore, dicta pariatur mollitia minus quasi
+	voluptatibus vitae ab dolore autem enim libero obcaecati ea!
+</p>
+
+<h3>Om egyptian mau</h3>
+<p>
+	Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem veniam
+	debitis voluptates corporis quaerat nesciunt, commodi inventore amet
+	recusandae deserunt blanditiis iure nobis possimus? Non sunt veritatis saepe
+	repellendus! Repudiandae.
+</p>
+
+<h3>Att köpa katt</h3>
+<p>
+	Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur ipsa rerum
+	itaque ipsam, assumenda corrupti modi. Tempora, nostrum qui officiis numquam
+	alias similique voluptate deserunt at ex voluptates aut sed?
+</p>
+
+<h3>Köpvillkor</h3>
+<p>
+	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque id similique
+	ipsum! Deleniti accusantium quaerat perferendis, explicabo recusandae vel
+	sapiente error. Non deserunt doloribus aperiam expedita. Incidunt, qui. Amet,
+	fuga!
 </p>
 
 <style>
