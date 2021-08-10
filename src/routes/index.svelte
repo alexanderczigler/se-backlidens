e39466178e101597b2495bbad8a2e7cab019e753
Alexander Czigler <alexander@iteam.se>
2021-08-10 16:50:58 +0200

    feat: a simple intro

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,40 +10,27 @@
 
 <h3>Välkommen!</h3>
 <p>
-	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab fugit voluptatum
-	quibusdam deserunt fuga, eligendi quos reiciendis laboriosam doloribus, harum
-	nulla a numquam, nam sapiente placeat tempora eos debitis tempore!
+	Vi är en kommande uppfödare av Egyptian Mau; en korthårig, elegant kattras som
+	sägs vara urprunget till den moderna tamkatten. Här på hemsidan kommer vi att
+	fylla på med information om hur egyptian Mau är som ras och vad du behöver
+	tänka på när du köper en kattunge av oss.
+</p>
+<p>
+	Vi planerar en första kull omkring oktober, är du nyfiken på att skaffa en mau
+	se till att kika in här på hemsidan och följa oss på Instagram!
 </p>
 
 <h3>Om oss</h3>
-<p>
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, modi explicabo
-	sint facilis ratione commodi inventore, dicta pariatur mollitia minus quasi
-	voluptatibus vitae ab dolore autem enim libero obcaecati ea!
-</p>
+<p>-</p>
 
 <h3>Om egyptian mau</h3>
-<p>
-	Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem veniam
-	debitis voluptates corporis quaerat nesciunt, commodi inventore amet
-	recusandae deserunt blanditiis iure nobis possimus? Non sunt veritatis saepe
-	repellendus! Repudiandae.
-</p>
+<p>-</p>
 
 <h3>Att köpa katt</h3>
-<p>
-	Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur ipsa rerum
-	itaque ipsam, assumenda corrupti modi. Tempora, nostrum qui officiis numquam
-	alias similique voluptate deserunt at ex voluptates aut sed?
-</p>
+<p>-</p>
 
 <h3>Köpvillkor</h3>
-<p>
-	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque id similique
-	ipsum! Deleniti accusantium quaerat perferendis, explicabo recusandae vel
-	sapiente error. Non deserunt doloribus aperiam expedita. Incidunt, qui. Amet,
-	fuga!
-</p>
+<p>-</p>
 
 <style>
 	@media (min-width: 480px) {
